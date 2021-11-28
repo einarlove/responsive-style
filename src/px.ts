26d02e2033c71c9cc47c399085762bc804923bf2
Einar Löve <git@einarlove.com>
2021-11-28 16:59:49 +0100

    Add comment for px helper function

--- src/px.ts
+++ src/px.ts
@@ -1,8 +1,13 @@
-export function px(input: string | number | undefined) {
-  switch (typeof input) {
+/**
+ * @param value A value to a CSS property that is to be converted to a pixel value. If the value is a number, it is assumed to be in pixels,
+ *  otherwise it is assumed to be a string that should be left untouched, like 5em, 10vw or 12px.
+ * @returns If value is a number, converted to a string with px appended. Otherwise, the input is returned untouched.
+ */
+export function px(value: string | number | undefined) {
+  switch (typeof value) {
     case 'number':
-      return input + 'px'
+      return value + 'px'
     default:
-      return input
+      return value
   }
 }
